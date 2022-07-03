eeb0170ef6e84d64e375820e144a282c0ba09937
kiyua <bestycf1@gmail.com>
2022-07-03 09:55:07 -0700

    five to nine

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * print alphabets a to z
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
