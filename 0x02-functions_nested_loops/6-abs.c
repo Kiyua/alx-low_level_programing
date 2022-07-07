58e450ec44928ef5ff23ae94de50fe8bb8207d58
kiyua <bestycf1@gmail.com>
2022-07-06 23:06:27 -0700

    six n eight

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - returns absolute value of a given number
+ *
+ * @n: int
+ * Return: absolute value of n
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,24 @@
  */
 void jack_bauer(void)
 {
-    int hour, min;
-    int hour1, hour2, min1, min2;
-    for (hour = 0; hour < 24; hour++)
-    {
-        hour1 = hour / 10;
-        hour2 = hour % 10;
-        for (min = 0; min < 60; min++)
-        {
-            min1 = min / 10;
-            min2 = min % 10;
-            
-            _putchar('0' + hour1);
-            _putchar('0' + hour2);
-            _putchar(':');
-            _putchar('0' + min1);
-            _putchar('0' + min2);
-            _putchar('\n');
-        }
-    }
+	int hour, min;
+	int hour1, hour2, min1, min2;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		hour1 = hour / 10;
+		hour2 = hour % 10;
+		for (min = 0; min < 60; min++)
+		{
+			min1 = min / 10;
+			min2 = min % 10;
+			_putchar('0' + hour1);
+			_putchar('0' + hour2);
+			 _putchar(':');
+			_putchar('0' + min1);
+			_putchar('0' + min2);
+			_putchar('\n');
+		}
+	}
 }
 
