ef706f828b579e7f9d77653e1195b5f15bc3c407
kiyua <bestycf1@gmail.com>
2022-07-08 14:21:57 -0700

    two and three

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - return multiple of 1
+ *
+ * @a: int
+ * @b: int
+ * Return: multiple of two ints
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
