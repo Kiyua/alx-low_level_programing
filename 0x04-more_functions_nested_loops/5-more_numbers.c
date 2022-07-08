c5f147a22b2c3c2edd92a1eff35c311bca91db3a
kiyua <bestycf1@gmail.com>
2022-07-08 15:00:18 -0700

    four and five

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints digits
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int x;
+	char c;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			int d;
+			d = c % 10;
+			if (c >= 10)
+			{
+				_putchar('1');
+			}
+			_putchar('0' + d);
+		}
+		_putchar('\n');
+	}
+}
+
