ad575a4a01b9beda26811316a049b6c0153adf37
kiyua <bestycf1@gmail.com>
2022-07-17 22:15:26 -0700

    two

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - func
+ *
+ * @s: char pointer
+ * @c: char
+ * Return: char
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
