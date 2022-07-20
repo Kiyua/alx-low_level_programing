a6ab4a179481fb683778ed89107eeaece1ae1af7
kiyua <bestycf1@gmail.com>
2022-07-19 17:16:52 -0700

    putchar

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - recursion
+ *
+ * @s: char
+ */
+void _puts_recursion(char *s)
+{
+
+	_putchar(*s);
+
+	if (*s != '\0')
+	{
+		_puts_recursion(++s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
+
+
