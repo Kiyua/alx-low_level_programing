7577435d36078ad4856165cef9f6df20ff6faf68
kiyua <bestycf1@gmail.com>
2022-07-20 10:04:23 -0700

    five

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _sqrt - func
+ * @n: int
+ * @x: int
+ *
+ * Return: int
+ */
+int _sqrt(int n, int x)
+{
+	if (x > (n / 2))
+		return (-1);
+	if (x * x == n)
+		return (x);
+	return (_sqrt(n, x + 1));
+}
+
+/**
+ * _sqrt_recursion - func
+ * @n: int
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
