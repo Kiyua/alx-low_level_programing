b96f1c1f0ddd23c2598396f78978f8685fbbc3cf
kiyua <bestycf1@gmail.com>
2022-07-26 16:50:00 -0700

    zero

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - func
+ * @size: unsigned int
+ * @c: char
+ * Return: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(c));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
+
