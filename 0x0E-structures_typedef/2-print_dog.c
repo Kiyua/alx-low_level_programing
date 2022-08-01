d330bc679ffb759971aa71e2a56e060233f59c75
kiyua <bestycf1@gmail.com>
2022-08-01 15:00:34 -0700

    two

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - func
+ *
+ * @d: dog
+ */
+void print_dog(struct dog *d)
+{
+	char *name, *owner;
+	float age;
+
+	name = d->name;
+	if (name == NULL)
+		name = "(nil)";
+	owner = d->owner;
+	if (owner == NULL)
+		owner = "(nil)";
+	age = d->age;
+
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", name, age, owner);
+}
