49f9f1dc7ae03c328c703dee32a0c18bc84a8b2d
kiyua <bestycf1@gmail.com>
2022-08-16 23:15:02 -0700

    nine

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - func
+ *
+ * @head: listint_t
+ * @index: int
+ * @n: int
+ *
+ * Return: listint_t
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+	listint_t *h, *temp;
+	listint_t *new = malloc(sizeof(listint_t));
+	unsigned int count = 0;
+
+	new->n = n;
+	h = *head;
+	while (head != NULL && h != NULL)
+	{
+		if (count == index)
+		{
+			temp = h->next;
+			h->next = new;
+			new->next = temp;
+
+			return (new);
+		}
+		h = h->next;
+		count++;
+	}
+
+	return (NULL);
+}
