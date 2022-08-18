e6aa3c173c0e4018b773782d650a5daf7d1920af
kiyua <bestycf1@gmail.com>
2022-08-17 17:36:21 -0700

    handle malloc

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,9 +12,16 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
-	listint_t *h = malloc(sizeof(listint_t));
+	listint_t *new, *h;
 	unsigned int count = 0;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	h = malloc(sizeof(listint_t));
+	if (h == NULL)
+		return (NULL);
 
 	new->n = n;
 	if (index == count)
