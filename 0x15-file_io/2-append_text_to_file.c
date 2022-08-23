3bd84ffe79f17cf98d62599026cfaa81d856d891
kiyua <bestycf1@gmail.com>
2022-08-23 16:54:05 -0700

    two

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,27 +9,27 @@
 #include <fcntl.h>
 
 /**
- * create_file - func
+ * append_text_to_file - func
  *
  * @filename: char
- * @content: char
+ * @text_content: char
  *
  * Return: int
  */
-int create_file(const char *filename, char *content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, count;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (content != NULL)
+	if (text_content != NULL)
 	{
-		count = write(fd, content, strlen(content));
+		count = write(fd, text_content, strlen(text_content));
 		if (count == -1)
 			return (-1);
 	}
