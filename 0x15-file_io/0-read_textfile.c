d951e067b6552643089d2d12ebfce62968a8ad22
kiyua <bestycf1@gmail.com>
2022-08-23 18:04:40 -0700

    fix 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,11 +24,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
-	if (fd < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
 	count = read(fd, buf, letters);
+	if (count == -1)
+		return (0);
+
 	buf[count] = '\0';
 
 	close(fd);
